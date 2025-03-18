4192777409429309494897fc7a8acbdcc7e52915
Tteean <nailato@code.edu.az>
2025-03-19 01:33:52 +0400

    feat: sql connection added

---
+++ FinalProject_DataAccess/TicsTubeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FinalProject_Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject_DataAccess
+{
+    public class TicsTubeDbContext : DbContext
+    {
+        DbSet<Setting> Settings { get; set; }
+        public TicsTubeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ FinalProject_Service/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FinalProject_Service
+{
+    public class Class1
+    {
+
+    }
+}
